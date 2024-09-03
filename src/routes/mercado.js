ce8389f8650c522f726fa6572d0c226885dd43a8
Priscila <pyea08@gmail.com>
2024-09-03 12:40:33 -0300

    feat: router mercado

---
+++ src/routes/mercado.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mercadoController = require("../controllers/mercado");
+const cep_endereco = require("../middlewares/cep_endereco");
+
+const router = express.Router();
+
+router.get("/", (req,res) =>{
+    res.json(mercadoController.index())
+})
+router.get("/:id", (req,res)=>{
+    res.json(mercadoController.show(req.params.id))
+})
+router.post("/", cep_endereco, (req,res)=>{
+    const code = mercadoController.create(req.body)
+    res.status(code).json
+})
+router.put("/:id", cep_endereco,  (req,res)=>{
+    const code = mercadoController.update(req.body, req.params.id)
+    res.status(code).json
+})
+router.delete("/:id", (req,res)=>{
+    res.json(mercadoController.destroy(req.params.id))
+})
+module.exports = router
