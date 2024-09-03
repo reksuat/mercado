d3785d0d0c55cf0b95efcbe6994a9af43e7c049f
Priscila <pyea08@gmail.com>
2024-09-03 14:39:36 -0300

    feat: update mercado router

--- src/routes/mercado.js
+++ src/routes/mercado.js
@@ -12,11 +12,11 @@ router.get("/:id", (req,res)=>{
 })
 router.post("/", cep_endereco, (req,res)=>{
     const code = mercadoController.create(req.body)
-    res.status(code).json
+    res.status(code).json(req.body)
 })
 router.put("/:id", cep_endereco,  (req,res)=>{
     const code = mercadoController.update(req.body, req.params.id)
-    res.status(code).json
+    res.status(code).json(req.body)
 })
 router.delete("/:id", (req,res)=>{
     res.json(mercadoController.destroy(req.params.id))
