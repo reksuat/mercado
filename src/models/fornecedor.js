f236269157c9e7bc981d3b5efd8be6e594c9e892
Priscila <pyea08@gmail.com>
2024-09-03 14:40:38 -0300

    feat: fornecedor modelo

---
+++ src/models/fornecedor.js
@@ -0,0 +1,14 @@
+module.exports = modelo;
+let proxId = 1;
+
+function modelo(body, id= proxId++) {
+        if (
+            body.nome != "" &&
+            body.nome != undefined
+        ) {
+            return {
+                id,
+                nome: body.nome
+            }
+        }
+}
